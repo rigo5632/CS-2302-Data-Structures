35096ff3cd77ece9acf34754818497f61ce65ba7
rigo5632 <41413610+rigo5632@users.noreply.github.com>
2019-02-08 21:04:10 -0700

    Update lab1d.py

--- lab1d.py
+++ lab1d.py
@@ -1,3 +1,11 @@
+# Lab 1
+# By: Rigobeto Quiroz
+# Class: 1:30 PM - 2:50 PM MW
+# This program will draw five circles inside and original circle,
+# then inside those circles, 5 new circles will be generated and so on.
+# Since we have 3 cricles horizontally and vertically, we have to
+# cut each section in thirds, affecting the radius and the center at the same time. 
+
 import matplotlib.pyplot as plt
 import numpy as np
 import math
@@ -11,11 +19,16 @@ def draw_circles(ax,n,a,b,radius,w):
     if n>0:
         x,y = circle(a,b,radius)
         ax.plot(x,y,color='k')
+        # Original and center circle
         draw_circles(ax,n-1,a,b,radius*w,w)
-        draw_circles(ax,n-1,a+(radius*.66),b,radius*.33,w)
-        draw_circles(ax,n-1,a-(radius*.66),b,radius*.33,w)
-        draw_circles(ax,n-1,a,b+(radius*.66),radius*.33,w)
-        draw_circles(ax,n-1,a,b-(radius*.66),radius*.33,w)
+        # Right Circle
+        draw_circles(ax,n-1,a+(radius*.66),b,radius*.w,w)
+        # Left Cricle
+        draw_circles(ax,n-1,a-(radius*.66),b,radius*.w,w)
+        # Top Circle
+        draw_circles(ax,n-1,a,b+(radius*.66),radius*.w,w)
+        # Bottom Circle
+        draw_circles(ax,n-1,a,b-(radius*.66),radius*.w,w)
 plt.close("all")
 fig, ax = plt.subplots()
 calls = int(input('Enter number of recursion calls: '))
